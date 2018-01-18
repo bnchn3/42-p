d570baef7417eadd07868686a1f3018ed267d7b9
Benjamin Chan <bchan@e1z2r5p43.42.us.org>
2018-01-18 14:27:33 -0800

    finished get_next_line, fixed issue in strlen

--- Projects/get_next_line/get_next_line.c
+++ Projects/get_next_line/get_next_line.c
@@ -5,69 +5,102 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bchan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/04 12:25:41 by bchan             #+#    #+#             */
-/*   Updated: 2018/01/05 14:00:11 by bchan            ###   ########.fr       */
+/*   Created: 2018/01/16 13:15:46 by bchan             #+#    #+#             */
+/*   Updated: 2018/01/18 14:26:11 by bchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		find_newline(char *buffer)
+static t_list	*get_file(int fd, t_list **file)
+{
+	t_list	*tmp;
+
+	tmp = *file;
+	while (tmp)
+	{
+		if ((int)tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew("\0", fd);
+	ft_lstadd(file, tmp);
+	tmp = *file;
+	return (tmp);
+}
+
+static int		find_newline(char *buffer)
 {
 	char	*result;
 	int		count;
 
-	count = 1;
+	count = ft_strlen(buffer);
 	result = ft_strchr(buffer, '\n');
 	if (result)
 	{
-		while (result != buffer)
+		while (*result != '\0')
 		{
-			result--;
-			count++;
+			result++;
+			count--;
 		}
-		return (count);
 	}
-	return (0);
+	return (count);
 }
 
-int		read_line(char *buffer, char **line, int br)
+static char		*read_line(char *buffer, char *line, int br)
 {
-	static int	total_br;
 	char		*temp;
 
-	temp = (char *)malloc(total_br + br);
-	if (temp && *line)
-		ft_memcpy(temp, *line, total_br);
-	if (*line)
-		free(*line);
-	*line = temp;
-	ft_memcpy(*line + total_br, buffer, br);
-	total_br += br;
-	return (0);
+	temp = ft_strnew(ft_strlen(line) + br);
+	if (temp && line)
+		ft_memcpy(temp, line, ft_strlen(line));
+	if (line)
+		free(line);
+	line = temp;
+	ft_memcpy(line + ft_strlen(line), buffer, br);
+	return (line);
 }
 
-int		get_next_line(const int fd, char **line)
+static char		*advance_line(int len, char *line)
 {
+	char	*temp;
+	int		i;
+
+	i = ft_strlen(line) - len - 1;
+	temp = ft_strnew(i);
+	if (temp && line)
+		ft_memcpy(temp, line + len + 1, i);
+	if (line)
+		free(line);
+	line = temp;
+	return (line);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_list	*file;
 	char			buffer[BUFF_SIZE];
 	int				br;
+	int				len;
+	t_list			*tmp;
 
-	br = 1;
-	while (br > 0)
+	if (fd < 0 || !line || read(fd, buffer, 0) < 0)
+		return (-1);
+	tmp = get_file(fd, &file);
+	*line = ft_strnew(1);
+	while ((br = read(fd, buffer, BUFF_SIZE)))
 	{
-		br = read(fd, buffer, BUFF_SIZE);
-		if (br == -1)
-			return (-1);
-		if (br > 0)
-		{
-			if (find_newline(buffer) == 0)
-				read_line(buffer, line, br);
-			else
-			{
-				read_line(buffer, line, find_newline(buffer) - 1);
-				return (1);
-			}
-		}
+		tmp->content = read_line(buffer, tmp->content, br);
+		if (ft_strchr(tmp->content, '\n'))
+			break ;
 	}
-	return (0);
+	if (br < BUFF_SIZE && ft_strlen(tmp->content) == 0)
+		return (0);
+	len = find_newline(tmp->content);
+	*line = read_line(tmp->content, *line, len);
+	if (len < (int)ft_strlen(tmp->content))
+		tmp->content = advance_line(len, tmp->content);
+	else
+		ft_strclr(tmp->content);
+	return (1);
 }
--- Projects/get_next_line/get_next_line.h
+++ Projects/get_next_line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: bchan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/04 12:18:46 by bchan             #+#    #+#             */
-/*   Updated: 2018/01/05 12:50:55 by bchan            ###   ########.fr       */
+/*   Updated: 2018/01/18 14:24:14 by bchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 32
 # include "libft/libft.h"
 # include <fcntl.h>
 
