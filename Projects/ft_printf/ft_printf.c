4324e02822e1d02859c2d2146cf472613bd1a6a7
Ben Chan <bnchn3@gmail.com>
2018-02-06 14:16:30 -0800

    streamlined code, moved to struct implementation and single string for printing

--- Projects/ft_printf/ft_printf.c
+++ Projects/ft_printf/ft_printf.c
@@ -12,6 +12,14 @@
 
 #include <stdarg.h>
 
+typedef struct	s_print
+{
+	char *flags;
+	int		width;
+	int		precision;
+	char	spec;
+}								t_print;
+
 int check_spec(char c)
 {
 	if (c == 'd' || c == 'i' || c == 'u' || c == 'o' || c == 'x' || c == 'X' ||
@@ -32,11 +40,8 @@ char get_spec(const char *format)
 	return (format[i]);
 }
 
-int number_spec(const char *format)
+int number_spec(char c)
 {
-	char c;
-
-	c = get_spec(format);
 	if (c == 'c' || c == 'C' || c == 's' || c == 'S' || c == 'p' || c == 'n')
 		return (0);
 	return (1);
@@ -66,7 +71,7 @@ char *apply_pound(char c, char *result)
 			c == 'g' || c == 'G')
 		return (force_dec(result));
 	if (c == 'd' || c == 'i')
-		return (ft_strdup(result));
+		return (result);
 	tmp1 = ft_strdup(result);
 	if ((c == 'o' || c == 'O') && result[0] != '0')
 		tmp2 = ft_strjoin("0", tmp1);
@@ -80,26 +85,21 @@ char *apply_pound(char c, char *result)
 	return (ft_strdup(result));
 }
 
-char *apply_flag(const char *format, char c, char *result)
+char *apply_flag(t_print form, char *result)
 {
 	char *temp;
-	int i;
 
-	i = 0;
 	temp = NULL;
-	while (!(check_spec(format[i])))
-		i++;
-	if (format[i] == 'd' || format[i] == 'i')
+	if (form->spec == 'd' || form->spec == 'i')
 	{
-		if (c == '+' && result[0] != '-')
+		if (ft_strchr(form->flags, '+') && ft_atoi(result) >= 0)
 			temp = ft_strjoin("+", result);
-		if (c == ' ' && result[0] != '-' && (!(ft_strchr(format, '+')) ||
-				ft_strchr(format, '+') > &(format[i])))
+		else if (ft_strchr(form->flags, ' ') && ft_atoi(result >= 0))
 			temp = ft_strjoin(" ", result);
 	}
-	if (c == '#' && number_spec(format))
-		temp = apply_pound(format[i], result);
-	if (temp)
+	if (ft_strchr(form->flags, '#') && number_spec(form->spec))
+		temp = apply_pound(form->spec, result);
+	if (temp != result)
 	{
 		free(result);
 		result = temp;
@@ -135,29 +135,21 @@ char *add_lead(char *result, char *temp, int wid)
 	return (s);
 }
 
-char *apply_width(const char *format, char *result, int wid)
+char *apply_width(t_print form, char *result)
 {
-	int left;
 	char *temp;
 	char *save;
 
-	left = 0;
-	if (wid <= (int)ft_strlen(result))
+	if (form->width <= (int)ft_strlen(result))
 		return (result);
-	temp = ft_strnew_char(wid - ft_strlen(result), ' ');
+	temp = ft_strnew_char(form->width - ft_strlen(result), ' ');
 	save = NULL;
-	while(*format != '%')
-	{
-		if (*format == '-')
-			left = 1;
-		if (*format == '0' && (number_spec(format)))
-			temp = ft_memset(temp, '0', ft_strlen(temp));
-		format--;
-	}
-	if (left == 1)
+	if (ft_strchr(form->flags, '0') && (number_spec(form->spec)))
+		temp = ft_memset(temp, '0', ft_strlen(temp));
+	if (ft_strchr(form->flags, '-'))
 		save = ft_strjoin(result, (temp = ft_memset(temp, ' ', ft_strlen(temp))));
 	else
-		save = add_lead(result, temp, wid - ft_strlen(result));
+		save = add_lead(result, temp, form->width - ft_strlen(result));
 	free(result);
 	free(temp);
 	return (save);
@@ -197,53 +189,34 @@ int find_digits(char *save)
 	return (count);
 }
 
-int find_flag(const char *wid, char c)
+void remove_zero(t_print form, char *save, char *result)
 {
-	while (*wid != '%')
-	{
-		if (*wid == c)
-			return (1);
-		wid--;
-	}
-	return (0);
-}
-
-void remove_zero(const char *wid, char *save, char *result)
-{
-	int left;
 	char *temp;
 
-	left = 0;
-	if (find_flag(wid, '-'))
-		left = 1;
 	ft_memmove(save, save + 1, ft_strlen(save));
-	if (ft_atoi(wid))
+	if (form->width)
 	{
 		temp = &(save[ft_strlen(save)]);
-		if (left == 1)
+		if (ft_strchr(form->flags, '-'))
 			ft_meminsert(temp, ' ', 1);
 		else
 			ft_meminsert(result, ' ', ft_strlen(result));
 	}
 }
 
-char *add_zero(const char *wid, char *save, char *result, int prec)
+char *add_zero(t_print form, char *save, char *result)
 {
-	int left;
 	char *temp;
 
-	left = 0;
-	if (find_flag(wid, '-'))
-		left = 1;
-	while (find_digits(save) + find_zeroes(save) < prec)
+	while (find_digits(save) + find_zeroes(save) < form->precision)
 	{
 		ft_meminsert(save, '0', ft_strlen(save));
-		if (ft_atoi(wid))
+		if (form->width)
 		{
 			temp = &(save[ft_strlen(save)]);
-			if (left == 1 && *(temp - 1) == ' ')
+			if (ft_strchr(form->flags, '-') && *(temp - 1) == ' ')
 				ft_memmove(temp - 1, temp, 1);
-			else if (left != 1 && *result == ' ')
+			else if (!(ft_strchr(form->flags, '-')) && *result == ' ')
 			{
 				ft_memmove(result, result + 1, ft_strlen(result));
 				save--;
@@ -255,78 +228,78 @@ char *add_zero(const char *wid, char *save, char *result, int prec)
 	return (save);
 }
 
-char *precision_int(char *result, const char *wid, int prec)
+char *precision_int(char *result, t_print form)
 {
 	char *save;
 
-	if (ft_atoi(result) == 0 && prec == 0)
+	if (ft_atoi(result) == 0 && form->precision == 0)
 		return (ft_memset(result, ' ', ft_strlen(result)));
 	save = result;
 	while (!(ft_isdigit(*save)))
 		save++;
-	if (find_digits(save) > prec)
+	if (find_digits(save) > form->precision)
 		while (find_zeroes(save))
-			remove_zero(wid, save, result);
-	else if (find_digits(save) + find_zeroes(save) > prec)
-		while (find_digits(save) + find_zeroes(save) > prec)
-			remove_zero(wid, save, result);
+			remove_zero(form, save, result);
+	else if (find_digits(save) + find_zeroes(save) > form->precision)
+		while (find_digits(save) + find_zeroes(save) > form->precision)
+			remove_zero(form, save, result);
 	else
-			result = add_zero(wid, save, result, prec);
+			result = add_zero(form, save, result);
 	return (result);
 }
 
-char *precision_oct(char *result, const char *wid, int prec)
+char *precision_oct(char *result, t_print form)
 {
 	char *save;
 
-	if (ft_atoi(result) == 0 && prec == 0)
+	if (ft_atoi(result) == 0 && form->precision == 0)
 		return (ft_memset(result, ' ', ft_strlen(result)));
 	save = result;
 	while (!(ft_isdigit(*save)))
 		save++;
-	if (find_flag(wid, '#'))
+	if (ft_strchr(form->flags, '#'))
 		save++;
-	if (find_digits(save) > prec)
+	if (find_digits(save) > form->precision)
 		while (find_zeroes(save))
-			remove_zero(wid, save, result);
-	else if (find_digits(save) + find_zeroes(save) > prec)
-		while (find_digits(save) + find_zeroes(save) > prec)
-			remove_zero(wid, save, result);
+			remove_zero(form, save, result);
+	else if (find_digits(save) + find_zeroes(save) > form->precision)
+		while (find_digits(save) + find_zeroes(save) > form->precision)
+			remove_zero(form, save, result);
 	else
-		result = add_zero(wid, save, result, prec);
+		result = add_zero(form, save, result);
 	return (result);
 }
 
-char *precision_hex(char *result, const char *wid, int prec)
+char *precision_hex(char *result, t_print form)
 {
 	char *save;
 
-	if (ft_atoi(result) == 0 && prec == 0)
+	if (ft_atoi(result) == 0 && form->precision == 0)
 		return (ft_memset(result, ' ', ft_strlen(result)));
 	save = result;
 	while (!(ft_isdigit(*save)))
 		save++;
-	if (find_flag(wid, '#'))
+	if (ft_strchr(form->flags, '#'))
 		save += 2;
-	if (find_digits(save) > prec)
+	if (find_digits(save) > form->precision)
 		while (find_zeroes(save))
-			remove_zero(wid, save, result);
-	else if (find_digits(save) + find_zeroes(save) > prec)
-		while (find_digits(save) + find_zeroes(save) > prec)
-			remove_zero(wid, save, result);
+			remove_zero(form, save, result);
+	else if (find_digits(save) + find_zeroes(save) > form->precision)
+		while (find_digits(save) + find_zeroes(save) > form->precision)
+			remove_zero(form, save, result);
 	else
-		result = add_zero(wid, save, result, prec);
+		result = add_zero(form, save, result);
 	return (result);
 }
 
-char *minimum_digits(char c, char *result, const char *wid, int prec)
+char *minimum_digits(char c, char *result, t_print form)
 {
 	if (c == 'd' || c == 'D' || c == 'i' || c == 'u' || c == 'U')
-			result = precision_int(result, wid, prec);
+			result = precision_int(result, form);
 	if (c == 'o' || c == 'O')
-			result = precision_oct(result, wid, prec);
+			result = precision_oct(result, form);
 	if (c == 'x' || c == 'X')
-			result = precision_hex(result, wid, prec);
+			result = precision_hex(result, form);
 	return (result);
 }
 
@@ -354,48 +327,32 @@ char *max_char(char *result, const char *wid, int prec, char *temp)
 	return (result);
 }
 
-char *apply_precision(char *temp, char *result, const char *wid,
-											int prec)
+char *apply_precision(char *temp, char *result, t_print form)
 {
 	char c;
 
-	c = get_spec(wid);
+	c = form->spec;
 	if (c == 'd' || c == 'D' || c == 'i' || c == 'u' || c == 'U' || c == 'o' ||
 			c == 'O' || c == 'x' || c == 'X')
-		result = minimum_digits(c, result, wid, prec);
+		result = minimum_digits(c, result, form);
 	else if (c == 's')
 		result = max_char(result, wid, prec, temp);
 	return (result);
 }
 
-char *modify_string(const char *format, char *result)
+char *modify_string(t_print form, char *result)
 {
-	int i;
-	const char *wid;
 	char *temp;
 
-	i = 1;
 	temp = ft_strdup(result);
-	while (format[i] == '+' || format[i] == ' ' || format[i] == '#' ||
-				format[i] == '-' || format[i] == '0')
-	{
-		result = apply_flag(format, format[i], result);
-		i++;
-	}
-	wid = &(format[i]);
-	if ((ft_atoi(wid)))
-	{
-		result = apply_width(wid, result, ft_atoi(wid));
-		while(format[i] >= '0' && format[i] <= '9')
-			i++;
-	}
-	if (format[i] == '.')
-		result = apply_precision(temp, result, wid, ft_atoi(&(format[i + 1])));
+	result = apply_flag(form, result);
+	result = apply_width(form, result);
+	result = apply_precision(temp, result, form);
 	free(temp);
 	return (result);
 }
 
-char *pull_int(const char *format, va_list ap, char c)
+char *pull_int(t_print form, va_list ap, char c)
 {
 	int arg;
 	char *result;
@@ -405,11 +362,11 @@ char *pull_int(const char *format, va_list ap, char c)
 		result = ft_itoa(arg);
 	else
 		result = ft_strdup(&arg);
-	result = modify_string(format, result);
+	result = modify_string(form, result);
 	return (result);
 }
 
-char *pull_un_int(const char *format, va_list ap, char c)
+char *pull_un_int(t_print form, va_list ap, char c)
 {
 	unsigned int arg;
 	char *result;
@@ -431,20 +388,20 @@ char *pull_un_int(const char *format, va_list ap, char c)
 		  i++;
 		}
 	}
-	result = modify_string(format, result);
+	result = modify_string(form, result);
 	return (result);
 }
 
-char *pull_string(const char *format, va_list ap)
+char *pull_string(t_print form, va_list ap)
 {
 	char *arg;
 
 	arg = ft_strdup(va_arg(ap, char *));
-	arg = modify_string(format, arg);
+	arg = modify_string(form, arg);
 	return (arg);
 }
 
-char *pull_voidp(const char *format, va_list ap)
+char *pull_voidp(t_print form, va_list ap)
 {
 	void *arg;
 	char *result;
@@ -454,51 +411,136 @@ char *pull_voidp(const char *format, va_list ap)
 	temp = ft_hex_convert((unsigned int)arg);
 	result = ft_strjoin("0x", temp);
 	free(temp);
-	result = modify_string(format, result);
+	result = modify_string(form, result);
 	return (result);
 }
 
-char *length_hh(const char *format, va_list ap, char c)
+char *pull_char(t_print form, va_list ap)
 {
-		if ()
+	char arg;
+	char *result;
+
+	arg = va_arg(ap, char);
+	result = ft_itoa((int)arg);
+	result = modify_string(form, result);
+	return (result);
 }
 
-char *length_l(const char *format, va_list ap, char c)
+char *pull_un_char(t_print form, va_list ap, char c)
+{
+	unsigned char arg;
+	char *result;
+
+	arg = va_arg(ap, unsigned char);
+	if (c == 'u')
+		result = ft_unsigned_itoa((unsigned int)arg);
+	if (c == 'o')
+		result = ft_octal_convert((unsigned int)arg);
+	if (c == 'x')
+		result = ft_hex_convert((unsigned int)arg);
+	if (c == 'X')
+	{
+		i = 0;
+		result = ft_hex_convert((unsigned int)arg);
+		while (result[i])
+		{
+			result[i] = (char)ft_toupper(result[i]);
+		  i++;
+		}
+	}
+	result = modify_string(form, result);
+	return (result);
+}
+
+char *length_hh(t_print form, va_list ap, char c)
+{
+		if (c == 'd' || c == 'i')
+			return (pull_char(form, ap));
+		if (c == 'u' || c == 'o' || c == 'x' || c == 'X')
+			return (pull_un_char(form, ap, c));
+		if (c == 'n')
+			return (pull_charp(form, ap));
+		return (length_none(form, ap ,c));
+}
+
+char *length_h(t_print form, va_list ap, char c)
+{
+	if (c == 'd' || c == 'i')
+		return (pull_short(form, ap));
+	if (c == 'u' || c == 'o' || c == 'x' || c == 'X')
+		return (pull_un_short(form, ap, c));
+	if (c == 'n')
+		return (pull_shortp(form, ap));
+	return (length_none(form, ap ,c));
+}
+
+char *length_l(t_print form, va_list ap, char c)
 {
 	if (c == 'd' || c == 'i' || c == 'D')
-		return (pull_l_int(format, ap, c));
+		return (pull_long(form, ap, c));
 	if (c == 'u' || c == 'U' || c == 'o' || c == 'O' || c == 'x' || c == 'X')
-		return (pull_un_l_int(format, ap, c));
+		return (pull_un_long(form, ap, c));
 	if (c == 'c' || c == 'C')
-		return (pull_wint(format, ap, c));
+		return (pull_wint(form, ap, c));
 	if (c == 's' || c == 'S')
-		return (pull_wchar(format, ap, c));
+		return (pull_wchar(form, ap, c));
 	if (c == 'n')
-		return (pull_l_intp(format, ap, c));
-	return (length_none(format, ap, c));
+		return (pull_longp(form, ap, c));
+	return (length_none(form, ap, c));
 }
 
-char *length_none(const char *format, va_list ap, char c)
+char *length_ll(t_print form, va_list ap, char c)
+{
+	if (c == 'd' || c == 'i')
+		return (pull_long_long(form, ap));
+	if (c == 'u' || c == 'o' || c == 'x' || c == 'X')
+		return (pull_un_long_long(form, ap, c));
+	if (c == 'n')
+		return (pull_long_longp(form, ap));
+	return (length_none(form, ap ,c));
+}
+
+char *length_j(t_print form, va_list ap, char c)
+{
+	if (c == 'd' || c == 'i')
+		return (pull_intmax(form, ap));
+	if (c == 'u' || c == 'o' || c == 'x' || c == 'X')
+		return (pull_uintmax(form, ap, c));
+	if (c == 'n')
+		return (pull_intmaxp(form, ap));
+	return (length_none(form, ap ,c));
+}
+
+char *length_z(t_print form, va_list ap, char c)
+{
+	if (c == 'd' || c == 'i' || c == 'u' || c == 'o' || c == 'x' || c == 'X')
+		return (pull_sizet(form, ap));
+	if (c == 'n')
+		return (pull_sizetp(form, ap));
+	return (length_none(form, ap ,c));
+}
+
+char *length_none(t_print form, va_list ap, char c)
 {
 	if (c == 'd' || c == 'i' || c == 'c')
-		return (pull_int(format, ap, c));
+		return (pull_int(form, ap, c));
 	if (c == 'u' || c == 'o' || c == 'x' || c == 'X')
-		return (pull_un_int(format, ap, c));
+		return (pull_un_int(form, ap, c));
 	if (c == 'f' || c == 'F' || c == 'e' || c == 'E' || c == 'g' || c == 'G' ||
 			c == 'a' || c == 'A')
-		return (pull_double(format, ap, c));
+		return (pull_double(form, ap, c));
 	if (c == 's')
-		return (pull_string(format, ap));
+		return (pull_string(form, ap));
 	if (c == 'p')
-		return (pull_voidp(format, ap));
+		return (pull_voidp(form, ap));
 	if (c == 'n')
-		return (pull_intp(format, ap, c));
+		return (pull_intp(form, ap, c));
 	if (c == 'D' || c == 'U' || c == 'O' || c == 'C' || c == 'S')
-		return(length_l(format, ap, c));
+		return(length_l(form, ap, c));
 	return (NULL);
 }
 
-char	*check_length(const char *format, va_list ap)
+char	*check_length(const char *format, va_list ap, t_print form)
 {
 	int i;
 
@@ -508,38 +550,65 @@ char	*check_length(const char *format, va_list ap)
 	if (format[i - 1] == 'h')
 	{
 		if (format[i - 2] == 'h')
-			return (length_hh(format, ap, format[i]));
-		return (length_h(format, ap, format[i]));
+			return (length_hh(form, ap, format[i]));
+		return (length_h(form, ap, format[i]));
 	}
 	if (format[i - 1] == 'l')
 	{
 		if (format[i - 2] == 'l')
-			return (length_ll(format, ap, format[i]));
-		return (length_l(format, ap, format[i]));
+			return (length_ll(form, ap, format[i]));
+		return (length_l(form, ap, format[i]));
 	}
 	if (format[i - 1] == 'j')
-		return (length_j(format, ap, format[i]));
+		return (length_j(form, ap, format[i]));
 	if (format[i - 1] == 'z')
-		return (length_z(format, ap, format[i]));
-	return (length_none(format, ap, format[i]));
+		return (length_z(form, ap, format[i]));
+	return (length_none(form, ap, format[i]));
 }
 
-size_t print_arg(const char *format, va_list ap)
+t_print new_form(void)
+{
+	t_print form;
+
+	form = (t_print)malloc(sizeof(t_print));
+	form->flags = ft_strdup("");
+	form->width = 0;
+	form->precision = 0;
+	form->spec = 0;
+	return (form);
+}
+
+t_print get_form(const char *format, t_print form)
+{
+	int i;
+
+	i = 1;
+	while (format[i] == '+' || format[i] == '-' || format[i] == ' ' ||
+				format[i] == '0' || format[i] == '#')
+		form->flags = ft_strpchar(&(form->flags), format[i++]);
+	if (ft_atoi(&(format[i])))
+	{
+		form->width = ft_atoi(&(format[i]));
+		while (ft_isdigit(format[i]))
+			i++;
+	}
+	if (format[i] == '.')
+		form->precision = ft_atoi(&(format[i + 1]));
+	form->spec = get_spec(format);
+	return (form);
+}
+
+char *print_arg(const char *format, va_list ap)
 {
 	char *result;
+	t_print form;
 
 	if (format[1] == '%')
-	{
-		ft_putchar('%');
-		return ((size_t)1);
-	}
-	result = check_length(format, ap);
-	if (result)
-	{
-		ft_putstr(result);
-		return (ft_strlen(result));
-	}
-	return (-1);
+		return (ft_strdup("%"));
+	form = new_form();
+	form = get_form(format, form);
+	result = check_length(format, ap, form);
+	return (result);
 }
 
 int ft_printf(const char *format, ...)
@@ -547,6 +616,7 @@ int ft_printf(const char *format, ...)
 	va_list ap;
 	char *result;
 	int	i;
+	char *temp;
 
 	result = 0;
 	i = 0;
@@ -555,13 +625,14 @@ int ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			result += print_arg(&(format[i++]), ap);
+			result = ft_strpstr(&result, temp = print_arg(&(format[i++]), ap));
+			free(temp);
 			while(!(check_spec(format[i++])))
 				if (format[i - 1] == '%')
 					break ;
 		}
 		else
-			result = ft_insert_char(result, format[i++], ft_strlen(result))
+			result = ft_strpchar(&result, format[i++]);
 	}
 	va_end(ap);
 	return (ft_strlen(result));
